0b5f74391b3c58b79e2b88c8734481173b4c7b24
Philippe Pepiot <phil@philpep.org>
2014-01-15 13:41:10 +0100

    bind: bind statistics

---
+++ pnp-templates/check_mk-bind.php
@@ -0,0 +1,37 @@
+<?php
+
+$i = 0;
+
+$special = array("SUCCESS", "TRANSFERT", "REFUSED", "NXRRSET", "NXDOMAIN");
+
+#
+# Request types per minute
+#
+$i++;
+$def[$i]     = "";
+$opt[$i]     = " --title \"Request types per minute\"";
+$ds_name[$i] = "Request types per minute";
+foreach ($this->DS as $KEY=>$VAL) {
+    if (!(in_array($VAL['NAME'], $special))) {
+        $def[$i] .= rrd::def("var".$KEY, $VAL['RRDFILE'], $VAL['DS'], "AVERAGE");
+        $def[$i] .= rrd::area("var".$KEY, rrd::color($KEY),$VAL['NAME'], 'STACK' );
+        $def[$i] .= rrd::gprint("var".$KEY, array("LAST","MAX","AVERAGE"), "%6.0lf".$VAL['UNIT']);
+    }
+}
+
+#
+# Requests per Minute
+#
+$i++;
+$def[$i]     = "";
+$opt[$i]     = " --title \"Request status per minute\"";
+$ds_name[$i] = "Request status per minute";
+foreach ($this->DS as $KEY=>$VAL) {
+    if (in_array($VAL['NAME'], $special)) {
+        $def[$i] .= rrd::def("var".$KEY, $VAL['RRDFILE'], $VAL['DS'], "AVERAGE");
+        $def[$i] .= rrd::area("var".$KEY, rrd::color($KEY),$VAL['NAME'], 'STACK' );
+        $def[$i] .= rrd::gprint("var".$KEY, array("LAST","MAX","AVERAGE"), "%6.0lf".$VAL['UNIT']);
+    }
+}
+
+?>
